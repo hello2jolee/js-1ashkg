c0182039c7757686f21065e81ac8cfdfa254a447
hello2jolee <jioun.lee.dev@gmail.com>
2021-02-18 21:42:28 +0900

    resize listener 적용

--- index.js
+++ index.js
@@ -4,15 +4,42 @@ const navElem = document.querySelector("#nav");
 const navItems = Array.from(navElem.children);
 const contentsElem = document.querySelector("#contents");
 const contentItems = Array.from(contentsElem.children);
-const offsetTops = contentItems.map(elem => {
-  const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
-  return [ofs - clh / 2, ofs + clh / 2];
-});
+// const getOffsetTops = (() => {
+//   let ofst = 0;
+//   let res = [];
+//   return () => {
+//     if (window.innerHeight === ofst) {
+//       return res;
+//     }
+//     ofst = window.innerHeight;
+//     res = contentItems.map(elem => {
+//       const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
+//       return [ofs - clh / 2, ofs + clh / 2];
+//     });
+//     return res;
+//   };
+// })();
+let offsetTops = [];
+const getOffsetTops = () => {
+  offsetTops = contentItems.map(elem => {
+    const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
+    return [ofs - clh / 2, ofs + clh / 2];
+  });
+};
+getOffsetTops();
 
 window.addEventListener("scroll", e => {
   const { scrollTop } = e.target.scrollingElement;
-  // do something
+  const targetIndex = offsetTops.findIndex(
+    ([from, to]) => scrollTop >= from && scrollTop < to
+  );
+  Array.from(navElem.children).forEach((c, i) => {
+    if (i !== targetIndex) c.classList.remove("on");
+    else c.classList.add("on");
+  });
 });
+
+window.addEventListener("resize", getOffsetTops);
 
 navElem.addEventListener("click", e => {
   const targetElem = e.target;
