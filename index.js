9b400d13c798be8456fae02a26f7bd490847c4ec
hello2jolee <jioun.lee.dev@gmail.com>
2021-02-18 23:02:47 +0900

    throttle

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 import "./style.css";
+import { throttle, debounce } from "./util";
 
 const navElem = document.querySelector("#nav");
 const navItems = Array.from(navElem.children);
 const contentsElem = document.querySelector("#contents");
 const contentItems = Array.from(contentsElem.children);
-// const getOffsetTops = (() => {
-//   let ofst = 0;
-//   let res = [];
-//   return () => {
-//     if (window.innerHeight === ofst) {
-//       return res;
-//     }
-//     ofst = window.innerHeight;
-//     res = contentItems.map(elem => {
-//       const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
-//       return [ofs - clh / 2, ofs + clh / 2];
-//     });
-//     return res;
-//   };
-// })();
+
 let offsetTops = [];
 const getOffsetTops = () => {
   offsetTops = contentItems.map(elem => {
@@ -28,18 +15,21 @@ const getOffsetTops = () => {
 };
 getOffsetTops();
 
-window.addEventListener("scroll", e => {
-  const { scrollTop } = e.target.scrollingElement;
-  const targetIndex = offsetTops.findIndex(
-    ([from, to]) => scrollTop >= from && scrollTop < to
-  );
-  Array.from(navElem.children).forEach((c, i) => {
-    if (i !== targetIndex) c.classList.remove("on");
-    else c.classList.add("on");
-  });
-});
+window.addEventListener(
+  "scroll",
+  throttle(e => {
+    const { scrollTop } = e.target.scrollingElement;
+    const targetIndex = offsetTops.findIndex(
+      ([from, to]) => scrollTop >= from && scrollTop < to
+    );
+    Array.from(navElem.children).forEach((c, i) => {
+      if (i !== targetIndex) c.classList.remove("on");
+      else c.classList.add("on");
+    });
+  }, 300)
+);
 
-window.addEventListener("resize", getOffsetTops);
+window.addEventListener("resize", debounce(getOffsetTops, 3000));
 
 navElem.addEventListener("click", e => {
   const targetElem = e.target;
